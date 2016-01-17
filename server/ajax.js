1beb1205e9d84ec63fb969a49c6d77b435284703
zhengliangliang <zhengliangliang@baidu.com>
2016-01-17 15:52:57 +0800

    start trying uploading files;

--- server/ajax.js
+++ server/ajax.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var config = require('../common/config');
 var md5 = require('md5');
+var utils = require('./util/utils');
 
 var tokens = {};
 
@@ -15,8 +16,34 @@ module.exports = {
             all files should be sent to tmp-files/ first, and then be moved to
             video
          */
-        app.post('/ajax/upload-file', function(req) {
+        app.post('/ajax/upload-file', function(req, res) {
             var body = req.body;
+
+            // ensure the request has a valid token hash (i.e. md5( tokenString ) )
+            if (body.tokenHash && tokens[body.tokenHash]) {
+                delete tokens[body.tokenHash];
+            } else {
+                res.send('no-token');
+                return;
+            }
+
+            // make sure the file content exists and it doesn't exceed the MAX file content size
+            if (body.content && body.content.length < config.maxFileContentLength) {
+
+                // get the file path from req
+                var filePath = fs.realpathSync(config.path.videos + body.filePath);
+
+                // ensure the file path is inside videos
+                if (filePath.indexOf(config.path.videos) != 0) {
+                    res.send('invalid-file-path');
+                    return;
+                }
+
+                // ensure the dir is ready
+                utils.mkdir(dirname(body.filePath));
+                fs.writeFileSync(filePath, body.content);
+                res.send('ok');
+            }
             console.log(body);
         });
 
--- server/util/utils.js
+++ server/util/utils.js
@@ -1,3 +1,11 @@
+var fs = require('fs');
+
 module.exports = {
     // some util functions
+    mkdir: function(path) {
+        if (!fs.existsSync(path)) {
+            this.mkdir(npath.dirname(path));
+            fs.mkdirSync(path);
+        }
+    }
 };
